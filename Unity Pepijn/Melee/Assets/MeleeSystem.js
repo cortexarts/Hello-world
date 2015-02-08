7140e4e50786f7e917e106992ce5b77662d827c2
Pepijn Boer <pepijnb13@gmail.com>
2015-02-08 14:32:39 +0100

    Melee
    probeer heir iest mee te doen het doen het niet zo goed

---
+++ Unity Pepijn/Melee/Assets/MeleeSystem.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var Damage : int = 50;
+var Distance : float;
+
+function Update ()
+{
+	if (Input.GetButtonDown("Fire1"))
+	{
+		var hit : RaycastHit;
+		if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), hit))
+		{
+			Distance = hit.distance;
+			hit.transform.SendMessage("ApplyDamage", Damage, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
---
+++ Unity Pepijn/Melee/Assets/enemyLogic.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var Health = 100;
+
+function Update ()
+{
+	if(Health <= 0)
+	{
+		Dead();
+	}
+}
+
+function ApplyDammage (TheDammage : int)
+{
+	Health = Health - TheDammage;
+}
+function Dead ()
+{
+	Destroy (gameObject);
+}
