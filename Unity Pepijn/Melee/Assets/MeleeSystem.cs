55f036b6f632f731a74e8247cccf54dcc417d094
JDreau <jasperdre@gmail.com>
2015-02-09 16:03:17 +0100

    JS to CS transfer with 1 error
    Melee.cs contains an error

---
+++ Unity Pepijn/Melee/Assets/MeleeSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeSystem : MonoBehaviour {
+	
+	
+	int Damage = 50;
+	float Distance;
+	
+	void  Update (){
+		if (Input.GetButtonDown("Fire1"))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), hit))
+			{
+				Distance = hit.distance;
+				hit.transform.SendMessage("ApplyDamage", Damage, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
---
+++ Unity Pepijn/Melee/Assets/enemyLogic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyLogic : MonoBehaviour {
+	
+
+	int Health = 100;
+
+	void  Update (){
+		if(Health <= 0)
+		{
+			Dead();
+		}
+	}
+	
+	void  ApplyDamage ( int TheDamage  ){
+		Debug.Log("in ApplyDamage");
+		Health = Health - TheDamage;
+	}
+	void  Dead (){
+		Destroy (gameObject);
+	}
+}
