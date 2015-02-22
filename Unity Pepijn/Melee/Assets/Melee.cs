d46e3b69921c584666c8711dab3029c5dc9e6e69
InnoCrator <marxandso@gmail.com>
2015-02-22 12:19:11 +0100

    fixed error of raycasthit

--- Unity Pepijn/Melee/Assets/Melee.cs
+++ Unity Pepijn/Melee/Assets/Melee.cs
@@ -1,31 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+			//if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), hit))
+
+
 public class Melee : MonoBehaviour {
-	
-	
+
 	int TheDamage = 50;
 	float Distance;
 	float MaxDistance = 1.5f;
-	int hit = RaycastHit;
-	
-	void  Update (){
-		if (Input.GetButtonDown("Fire1"))
-		{
-		//ERROR
-			RaycastHit hit;
-			//END
-			if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), hit))
+
+	void Update() {
+		RaycastHit hit;
+
+		if (Input.GetButtonDown("Fire1")){
+
+			if (Physics.Raycast (transform.position, -Vector3.up, out hit))
 			{
 				Distance = hit.distance;
 				if (Distance < MaxDistance) 
-				{
-					hit.transform.SendMessage("ApplyDamage", TheDamage, SendMessageOptions.DontRequireReceiver);
+			{
+			hit.transform.SendMessage("ApplyDamage", TheDamage, SendMessageOptions.DontRequireReceiver);
 				}
 			}
 		}
 	}
-	
-	
-	
 }
