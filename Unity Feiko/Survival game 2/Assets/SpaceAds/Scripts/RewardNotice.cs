6a05870c355c3329f48cc4f3ca6c21e92dd5be27
henkiepenkie <feiko60@gmail.com>
2015-04-16 18:42:01 +0200

    Fancy animaties enzo
    In het mapje SpaceAds zitten animaties van Unity zelf, STELEUHHH

---
+++ Unity Feiko/Survival game 2/Assets/SpaceAds/Scripts/RewardNotice.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RewardNotice : MonoBehaviour 
+{
+	public Text text;
+	public Animator animator;
+
+	void Awake ()
+	{
+		SpaceAdsDemo.OnCoinsAddedAction += SetAmountAndShow;
+	}
+
+	void OnDestroy ()
+	{
+		SpaceAdsDemo.OnCoinsAddedAction -= SetAmountAndShow;
+	}
+
+	public void SetAmountAndShow (int amount)
+	{
+		string notice = text.text;
+		text.text = string.Format(notice,amount);
+
+		animator.Play("RewardNoticeVisible");
+	}
+}
