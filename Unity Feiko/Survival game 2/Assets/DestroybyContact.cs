7a9a09c189f425a08c812edde662a9202e9a52cf
Feiko van Dijk <feikovandijk@live.nl>
2015-11-01 14:01:28 +0100

    explosion bs

---
+++ Unity Feiko/Survival game 2/Assets/DestroybyContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroybyContact : MonoBehaviour {
+	
+	public GameObject explosion;
+	
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
