af934b1cbe75581617d8ed85ad73eb9a012a0a13
Feiko van Dijk <feikovandijk@live.nl>
2015-11-08 13:26:41 +0100

    project is nu echt weg, no worries.

--- Unity Feiko/Survival game 2/Assets/SpaceAds/Scripts/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Constants 
-{
-	public const string KEY_COINCOUNT = "CoinCount";
-
-	public const string KEY_REWARD_COOLDOWN_TIME = "RewardCooldownTime";
-
-	public const string KEYFORMAT_LEVEL_UNLOCKED = "Level{0}_Unlocked";
-	public const string KEYFORMAT_LEVEL_HIGHSCORE = "Level{0}_HighScore";
-}
